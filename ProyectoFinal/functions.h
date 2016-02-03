2125a501278b30817849de82ae92007da4a45922
Alejandro Alcalde <algui91@gmail.com>
2016-02-03 08:22:44 +0100

    Added documentation to function headers

--- ProyectoFinal/functions.h
+++ ProyectoFinal/functions.h
@@ -13,18 +13,61 @@
 
 using namespace std;
 
-
+/**
+ * 
+ * @param image
+ * @param channels
+ */
 void getRGBMatrix(cv::Mat &image, vector<cv::Mat> &channels);
 
+/**
+ * 
+ * @param source
+ * @param dest
+ * @param mask
+ * @return 
+ */
 cv::Mat solVector(cv::Mat &source, cv::Mat &dest, cv::Mat &mask);
 
+/**
+ * 
+ * @param sourceChannel
+ * @param x
+ * @param y
+ * @return 
+ */
 int guidanceVect(cv::Mat &sourceChannel, float x, float y);
 
+/**
+ * 
+ * @param source
+ * @param dest
+ * @param mask
+ * @param index
+ * @return 
+ */
 cv::Mat CoefficientMatrix(cv::Mat &source, cv::Mat &dest, cv::Mat &mask, cv::Mat &index);
 
 //---------------------------
+/**
+ * 
+ * @param r
+ * @param g
+ * @param b
+ * @param mask
+ * @param dest
+ * @param indexes
+ * @return 
+ */
 cv::Mat reconstructImage(cv::Mat &r, cv::Mat &g, cv::Mat &b, cv::Mat &mask, cv::Mat &dest, cv::Mat &indexes);
 
+/**
+ * 
+ * @param mask
+ * @param cols
+ * @param rows
+ * @return 
+ */
 cv::Mat getIndexes(cv::Mat &mask, int cols, int rows);
 
 #endif	/* FUNCTIONS_H */
