0711e662e2f1d0e8ae706b2412f306e658d22ef7
Alejandro Alcalde <algui91@gmail.com>
2016-02-05 18:17:06 +0100

    Adding mouse handler

--- ProyectoFinal/main.cpp
+++ ProyectoFinal/main.cpp
@@ -11,17 +11,41 @@
 #include "functions.h"
 
 using namespace std;
+using namespace cv;
 
 
-int var1 = 1;
-cv::Point pos;
-int radius = 7;
+Point point1, point2; /* vertical points of the bounding box */
+int drag = 0;
+Rect rect; /* bounding box */
+Mat img, roiImg; /* roiImg - the part of the image in the bounding box */
+int select_flag = 0;
 
-void CallBackFunc(int event, int x, int y, int flags, void* userdata) {
-    if (event == cv::EVENT_LBUTTONDOWN) {
-        pos.x = x;
-        pos.y = y;
-        var1 = 0;
+void mouseHandler(int event, int x, int y, int flags, void* param) {
+    if (event == CV_EVENT_LBUTTONDOWN && !drag) {
+        /* left button clicked. ROI selection begins */
+        point1 = Point(x, y);
+        drag = 1;
+    }
+
+    if (event == CV_EVENT_MOUSEMOVE && drag) {
+        /* mouse dragged. ROI being selected */
+        Mat img1 = img.clone();
+        point2 = Point(x, y);
+        rectangle(img1, point1, point2, CV_RGB(255, 0, 0), 3, 8, 0);
+        imshow("image", img1);
+    }
+
+    if (event == CV_EVENT_LBUTTONUP && drag) {
+        point2 = Point(x, y);
+        rect = Rect(point1.x, point1.y, x - point1.x, y - point1.y);
+        drag = 0;
+        roiImg = img(rect);
+    }
+
+    if (event == CV_EVENT_LBUTTONUP) {
+        /* ROI selected */
+        select_flag = 1;
+        drag = 0;
     }
 }
 
@@ -35,16 +59,21 @@ int main(int argc, char** argv) {
     cv::Mat mask = cv::imread("imagenes/bn4.png", cv::IMREAD_GRAYSCALE);
     mask = mask > 128;
 
-    //select the region on the image
-    while (var1 == 1) {
-        //set the callback function for any mouse event
-        cv::setMouseCallback("Video", CallBackFunc, NULL);
 
-        //show the image
-        cv::imshow("Video", im1);
-
-        // Wait until user press some key
-        cv::waitKey(10);
+    int k;
+    imshow("image", im1);
+    while (1) {
+        cap >> img;
+        cvSetMouseCallback("image", mouseHandler, NULL);
+        if (select_flag == 1) {
+            imshow("ROI", roiImg); /* show the image bounded by the box */
+        }
+        rectangle(img, rect, CV_RGB(255, 0, 0), 3, 8, 0);
+        imshow("image", img);
+        k = waitKey(10);
+        if (k == 27) {
+            break;
+        }
     }
 
 
