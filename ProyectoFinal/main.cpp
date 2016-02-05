f9cbbf7c8fd071ced326f478dec41741e8f1c406
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-02-06 00:33:13 +0100

    ading shark image result

--- ProyectoFinal/main.cpp
+++ ProyectoFinal/main.cpp
@@ -18,15 +18,15 @@ using namespace std;
 int main(int argc, char** argv) {
 
 
-    cv::Mat im1 = cv::imread("imagenes/dest_desert.jpg");
-    cv::Mat im2 = cv::imread("imagenes/shark_src.jpeg");
-    cv::Mat mask = cv::imread("imagenes/shark_mask.jpeg", cv::IMREAD_GRAYSCALE);
+    cv::Mat im1 = cv::imread("imagenes/dest_etsiit2.jpg");
+    cv::Mat im2 = cv::imread("imagenes/aleta11_src.png");
+    cv::Mat mask = cv::imread("imagenes/aleta11_mask2.png", cv::IMREAD_GRAYSCALE);
     mask = mask > 128;
 
     im1.convertTo(im1, CV_64FC3);
     im2.convertTo(im2, CV_64FC3);
 
-    cv::Mat result2 = seamlessClonningMixin(im2, im1, mask, cv::Point(350, 150));
+    cv::Mat result2 = seamlessClonningMixin(im2, im1, mask, cv::Point(150, 380));
     cv::imshow("mixed seamless cloning", result2);
     cv::imwrite("results/desert_shell.jpg", result2);
     cv::waitKey(0);
